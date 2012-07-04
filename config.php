f09fb7876f300ca159f661e48e14876d9e9e144d
drachehavoc <daniel.andrade.varela@gmail.com>
2012-07-04 13:33:21 -0300

    ...

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php // ♣
+
+	// BASE DIR
+	$options['basedir'] = "../_image-test";
+
+	// DEFAULT IMAGE SIZE
+	$options['size']['width']  = 200;
+	$options['size']['height'] = 150;
+	
+	// IF IMAGE IS NOT FOUND
+	$options['notfound']['path']   = "notFound.jpg";
+	$options['notfound']['width']  = 200;
+	$options['notfound']['height'] = 150;
+	
--- image.php
+++ image.php
@@ -1,33 +1,48 @@
 <?php // ♣
-
-	// http://127.0.0.1/acetenis/image/adm/uploads/gallery/1/Desert_-_200_x_200.jpg
-		
-	$x = @$_GET['x'];
-	$y = @$_GET['y'];
-	$ext = @$_GET['ext'];
-	$file = "../" . @$_GET['file'] . ".$ext";
 	
-	if( file_exists($file) ){
+	require_once 'config.php';
+	
+	$x     = ( isset($_GET['x']   ) ) ? $_GET['x']    : $options['size']['width']  ;
+	$y     = ( isset($_GET['y']   ) ) ? $_GET['y']    : $options['size']['height'] ;
+	$ext   = ( isset($_GET['ext'] ) ) ? $_GET['ext']  : ""                         ;
+	$fName = ( isset($_GET['file']) ) ? $_GET['file'] : ""                         ;
+	
+	$file = utf8_decode( "{$options['basedir']}/$fName.$ext" );
+
+	// Caso o arquivo exista no caminho especificado
+	if( !empty($fName) && !empty($ext) && file_exists($file) ){
+		// Gera Hash MD5 do arquivo especificado
 		$md5_filename = md5_file( $file );
+		
+		// Monta path da imagem buffer
 		$fileBuff = "buffer/{$md5_filename}_-_{$x}_x_{$y}.$ext";
 	
+		// Se não existe um arquivo já gerado para a 
+		// imagem e tamanho solicitado cria um e retorna
 		if( !file_exists($fileBuff) ){
 			require_once 'phpthumb/ThumbLib.inc.php';
 			$thumb = PhpThumbFactory::create($file);
 			$thumb->adaptiveResize($x, $y);
 			$thumb->show();
 			$thumb->save($fileBuff);
-		}else{
+		}
+		
+		// Se existe um arquivo já criado para a 
+		// imagem e tamanho solicitado, redireciona para ele
+		else{
 			$thisDir = basename( dirname($_SERVER['SCRIPT_FILENAME']) );
 			$uri = $_SERVER['REQUEST_URI'];
 			$goTo = preg_replace("/\/.*/", "", strtolower($_SERVER['SERVER_PROTOCOL'])) ."://". $_SERVER['HTTP_HOST'] . substr($uri, 0, strpos($uri, $thisDir)).$thisDir."/$fileBuff";
 			header("Location: $goTo");
 		}
-	}else{
-		$x = ( empty($x) )? 150 : $x;
-		$y = ( empty($y) )? 150 : $y;
+	}
+	
+	// Caso o arquivo não exista no caminho especificado
+	else{
+		$x = ( empty($x) ) ? $options['notfound']['width']  : $x;
+		$y = ( empty($y) ) ? $options['notfound']['height'] : $y;
 		require_once 'phpthumb/ThumbLib.inc.php';
-		$thumb = PhpThumbFactory::create("notFound.jpg");
+		$thumb = PhpThumbFactory::create($options['notfound']['path']);
 		$thumb->adaptiveResize($x, $y);
 		$thumb->show();
 	}
